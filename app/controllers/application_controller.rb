c47c8a0ce54db64ef9b8400cc8d02fddd1a512c3
Hethe Berg <growthcode@gmail.com>
2015-08-18 19:08:05 -0500

    add navbar functionality and style it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_plans #unless current_user?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def set_plans
+    unless current_user.blank?
+      @plans = current_user.plans
+    end
+  end
 end
