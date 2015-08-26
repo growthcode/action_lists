77872ca1c20211ce0fb6f20a10816f6a081cd43d
Hethe Berg <growthcode@gmail.com>
2015-08-26 14:28:58 -0500

    create tasks_wizard controller and 6 actions for steps

---
+++ app/controllers/tasks_wizard_controller.rb
@@ -0,0 +1,26 @@
+class TasksWizardController < ApplicationController
+
+  # step 1, identify tasks
+  def list
+  end
+
+  # step 2, rate importance level of tasks
+  def rate
+  end
+
+  # step 3, assign a position or person to do action
+  def assign
+  end
+
+  # step 4, decide what tasks will be included per position/person, reviewing overall workload
+  def include
+  end
+
+  # step 5, arrange order of tasks execution
+  def position
+  end
+
+  # step 6, give todos for tasks which are included but not ready
+  def preparation
+  end
+end
---
+++ spec/controllers/tasks_wizard_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe TasksWizardController, type: :controller do
+
+end
